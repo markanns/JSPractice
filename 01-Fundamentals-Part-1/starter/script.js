351ce30231889401e2cfcf17cd3951da137604fc
Marko Kostresevic <kostres90@hotmail.com>
2020-11-08 18:59:17 +0100

    added sunctionality

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+// const markWeights = 78;
+// const markHeight = 1.69;
+// const johnWeights = 92;
+// const johnHeight = 1.95;
+
+// const bmiMark = Math.round(markWeights / markHeight ** 2);
+// const bmiJohn = Math.round(johnWeights / johnHeight ** 2);
+// // const markHigherBMI = bmiMark > bmiJohn;
+
+// // let higner  ;
+
+// if (bmiMark > bmiJohn) {
+//     console.log('mark has biger BMI');
+// } else {
+//     console.log('john has biger BMI');
+// }
+
+const age = 23;
+
+age > 24 ? console.log('vece je') : console.log('nije vece');
+
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,30 @@
+// const calcAverage = (firstScore, secondScore, thirdScore) => {
+//     return average = (firstScore + secondScore + thirdScore) / 3;
+// }
+
+// const dolphins = calcAverage(44, 23, 71);
+// const koalas = calcAverage(65, 54, 49);
+
+// function checkWiner(avrgKoalas, avrgDolphins) {
+//     if (avrgDolphins >= 2 * avrgKoalas) {
+//         console.log('doplhin wins');
+//     } else if (avrgKoalas >= 2 * avrgDolphins) {
+//         console.log('koala wins');
+//     } else {
+//         console.log('niko');
+//     }
+// }
+
+// checkWiner(dolphins, koalas);
+
+// function calcTip(bill) {
+//     if (bill >= 50 && bill <= 300) {
+//         return (bill * 15) / 100
+//     } else return (bill * 20) / 100
+// }
+// const bils = [125, 555, 44];
+// const tips = [];
+// tips.push(calcTip(bils[0]));
+// tips.push(calcTip(bils[1]));
+// tips.push(calcTip(bils[2]));
+// console.log(tips);
